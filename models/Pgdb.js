276ad08c6fb0e300e34e238f44967fe633414085
Kristjan Gannon <kgannon@sovrn.com>
2018-09-18 18:04:13 -0600

    Initial commit

---
+++ models/Pgdb.js
@@ -0,0 +1,7 @@
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/pickem';
+const pg = require('pg');
+
+const client = new pg.Client(connectionString);
+client.connect();
+const query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', () => { client.end(); });
